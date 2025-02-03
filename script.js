52c9ebae2fba7fb0b97fd729c7c6235445e11a56
Ansh Mani Tripathi <144388813+anshggss@users.noreply.github.com>
2025-02-03 20:36:15 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the form from submitting
-    alert('Thank you for your message!');
-    this.reset(); // Reset the form
-});
